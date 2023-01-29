2e9b92527e9a1fda3775f1d597c7dd95f285de74
AmirBayat0 <Amirbayat.dev@gmail.com>
2023-01-29 17:13:21 +0330

    Examlpe 2 Added

--- flutter_bloc_example/lib/main.dart
+++ flutter_bloc_example/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../examples/persons_list.dart';
+import '../examples/fetch_data.dart';
 import '../examples/random_name.dart';
 
 void main() => runApp(
-      const MaterialApp(
+      MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Bloc Example',
-        home: RandomName(),
+        home: BlocProvider(
+          create: (BuildContext context) => PersonsBloc(),
+          child: const FetchData()
+          ,
+        ),
       ),
     );
